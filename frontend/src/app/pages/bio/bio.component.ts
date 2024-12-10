f48daf086af91c2c1461fb504f9afd2d76fac34a
TiagoAltstadt <tiagoaltstadt@gmail.com>
2024-12-10 15:56:52 +0100

    Improved Bio and Home

--- frontend/src/app/pages/bio/bio.component.ts
+++ frontend/src/app/pages/bio/bio.component.ts
@@ -7,22 +7,24 @@ import { Component } from '@angular/core';
 })
 export class BioComponent {
   technologies = [
-    'Node Js',
-    'Creation of APIs',
-    'Sequelize',
-    'C++',
-    'Git',
-    'React',
-    'Angular',
-    'Express',
-    'MVC Architecture',
-    'OOP',
-    'Postman',
-    'Javascript',
-    'MySQL',
-    'VSCode',
-    'Scrum',
-    'HTML and CSS'
+    { name: 'Angular', link: 'https://angular.io/' },
+    { name: 'HTML and CSS', link: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
+    { name: 'Node Js', link: 'https://nodejs.org/' },
+    { name: 'Git', link: 'https://git-scm.com/' },
+    { name: 'Javascript', link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
+    { name: 'Postman', link: 'https://www.postman.com/' },
+    { name: 'Typescript', link: 'https://developer.mozilla.org/en-US/docs/Glossary/TypeScript' },
+    { name: 'Express', link: 'https://expressjs.com/' },
+    { name: 'Creation of APIs', link: 'https://developer.mozilla.org/en-US/docs/Web/API' },
+    { name: 'Sequelize', link: 'https://sequelize.org/' },
+    { name: 'C++', link: 'https://en.cppreference.com/w/' },
+    { name: 'React', link: 'https://reactjs.org/' },
+    { name: 'MVC Architecture', link: 'https://developer.mozilla.org/en-US/docs/Glossary/MVC' },
+    { name: 'OOP', link: 'https://www.geeksforgeeks.org/object-oriented-programming-oops-concept-in-java/' },
+    { name: 'MySQL', link: 'https://www.mysql.com/' },
+    { name: 'VSCode', link: 'https://code.visualstudio.com/' },
+    { name: 'Scrum', link: 'https://www.scrum.org/' },
 ];
+
 
 }
